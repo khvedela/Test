8d022b6639d4db54f82d6637d206daa12b93e340
david khvedelidze <d.khv08@gmail.com>
2022-11-16 15:33:41 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import About from './components/About'
+import Landing from './components/Landing'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Landing />
+      <About />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Landing.tsx
@@ -0,0 +1,30 @@
+import gsap from 'gsap';
+import { useLayoutEffect, useRef } from 'react';
+import logo from '../assets/logo.svg'
+
+function Landing() {
+
+    const comp = useRef();
+
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.fromTo(".main-text", { y: "+=360", opacity: 0}, {y: "0", opacity: 1, duration: .5, ease: "ease-in"});
+            gsap.fromTo(".logo", { x: "+=360", opacity: 0}, {x: "0", opacity: 1, duration: 1, ease: "ease-in"})
+            gsap.fromTo(".learn-more", { x: "+=360", opacity: 0}, {x: "0", opacity: 1, duration: 1, ease: "ease-out", delay: 1.5})
+        }, comp)
+
+        return () => ctx.revert();
+    }, []);
+
+    return (
+        <div className="flex items-center justify-center h-screen w-screen bg-landing bg-center bg-cover from-indigo-500 backdrop-blur-md" ref={comp}>
+            <div className='flex flex-col gap-10'>
+                <h1 className="main-text text-8xl antialiased font-bold text-white">Explore Georgia <br /> With Me</h1>
+                <button className='learn-more focus:outline-none text-white bg-green-900 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>Learn more</button>
+            </div>
+            <img src={logo} className="logo w-1/3 h-1/3 tablet:hidden desktop:block" />
+        </div>
+    )
+}
+
+export default Landing
